0a0ee371d4f3f2864f10c182617f5884ecb8487a
Bhimesh <bhimeshdevarla999@gmail.com>
2022-09-29 15:34:33 +0530

    added

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const hello = () => {
-    console.log('Hello Node.Js! Trying it for the first time');
-}
+const http = require("http");
+const fs = require("fs");
+const args = (process.argv.slice(2));
 
-hello();
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html",(err,registration)=>{
+    if(err){
+        throw err;
+    }
+    registrationContent = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+    case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    
+    }
+  })
+  .listen(parseInt(args[0].slice(7)));
